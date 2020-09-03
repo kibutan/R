de60693855892760ebee2c8931522a7b0f59cf83
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2020-09-03 14:45:39 +0900

    Update minimum.R

--- kintone/minimum.R
+++ kintone/minimum.R
@@ -1,6 +1,3 @@
-#とあるkintoneアプリNo123から、今日現在1000行ほどのデータを読み込むスクリプト。
-#https://blog.cybozu.io/entry/2016/11/28/080000 を参照のこと。
-#https://www.r3it.com/blog/powerbi-with-r-script の参照も良い。
 library(kntnr)
 dataset<-kntn_records(app=123)
 as.data.frame(dataset)
